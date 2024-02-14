d6471f5b6b86b0d44cca10543b0b2f57bac6f0f2
Mia Mcintosh <miamcintosh636@gmail.com>
2024-02-14 18:40:39 +1100

    Window Handler added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{thread, time::{Duration, Instant}};
+use glium::Surface;
+use winit::platform::pump_events::PumpStatus;
+use crate::window::Window;
+
+mod window;
 fn main(){
-    println!("Lets do this")
+    let mut window = Window::new();
+    let now = Instant::now();
+    loop {
+        let frame = window.new_frame();
+
+        frame.clear_color(0.0, now.elapsed().as_secs_f32(), 0.0, 1.0);
+
+        if let PumpStatus::Exit(_) = window.update(){
+            break;
+        }
+
+        thread::sleep(Duration::from_millis(16));
+    }
 }
