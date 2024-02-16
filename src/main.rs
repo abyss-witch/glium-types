d52937b98b0ccc6bba1b161af32f5192d511b689
Mia Mcintosh <miamcintosh636@gmail.com>
2024-02-16 13:10:32 +1100

    Added shader and mesh handligng

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 use std::{thread, time::{Duration, Instant}};
-use glium::Surface;
+use glium::{index::{self, PrimitiveType}, Surface};
+use window::vec2;
 use winit::platform::pump_events::PumpStatus;
-use crate::window::Window;
+use crate::window::{Colour, Window};
 
+mod shaders;
 mod window;
 fn main(){
     let mut window = Window::new();
     let now = Instant::now();
+    const ONE_60TH_OF_A_SECOND: Duration = Duration::from_millis(1000/60);
+    
+    let shape = vec![vec2(-0.5, -0.5), vec2(0.0, 0.5), vec2(0.5, -0.25)];
+    let indices = index::NoIndices(PrimitiveType::TrianglesList).into();
+    let mesh = window.gen_mesh(&shape, indices);
+
+    let shader = shaders::SolidColour::new(Colour::new(0.0, 0.5, 0.6, 1.0), &window);
     loop {
+        //using with glium
         let frame = window.new_frame();
-
         frame.clear_color(0.0, now.elapsed().as_secs_f32(), 0.0, 1.0);
+        
+        window.draw_with_shader(&mesh, &shader);
 
-        if let PumpStatus::Exit(_) = window.update(){
-            break;
-        }
-
-        thread::sleep(Duration::from_millis(16));
+        if let PumpStatus::Exit(_) = window.update() { break; }
+        thread::sleep(ONE_60TH_OF_A_SECOND);
     }
 }
--- src/window.rs
+++ src/window.rs
@@ -1,14 +1,16 @@
 use std::time::Duration;
 
-use glium::{glutin::surface::WindowSurface, Display, Frame};
+use glium::{glutin::surface::WindowSurface, implement_uniform_block, implement_vertex, index, uniforms::{AsUniformValue, Uniforms}, Display, DrawParameters, Frame, Program, Surface, Vertex, VertexBuffer};
 use winit::platform::pump_events::{EventLoopExtPumpEvents, PumpStatus};
+
+use crate::shaders::Shader;
 
 #[allow(dead_code)]
 ///Used to create a window and draw things to it using the **glium** library. In the future it will suport inputs.
 pub struct Window{
     window: winit::window::Window,
     event_loop: winit::event_loop::EventLoop<()>,
-    display: Display<WindowSurface>,
+    pub display: Display<WindowSurface>,
     frame: Option<Frame>
 }
 impl Window{
@@ -45,4 +47,92 @@ impl Window{
         });
         status
     }
+    ///Create vertex buffer from shape.
+    pub fn vertex_buffer<V: Vertex>(&self, shape: &Vec<V>) -> VertexBuffer<V> {
+        glium::VertexBuffer::new(&self.display, shape).unwrap()
+    }
+
+    ///Generates a mesh using vertices and indices.
+    /// ```no_run
+    /// use Window::vec2;
+    /// 
+    /// let shape = vec![vec2(-0.5, -0.5), vec2(0.0, 0.5), vec2(0.5, -0.25)];
+    /// let indices = index::NoIndices(PrimitiveType::TrianglesList);
+    /// 
+    /// let mesh = window.gen_mesh(&shape, indices.into());
+    /// ```
+    pub fn gen_mesh<'a, V: Vertex>(&self, shape: &Vec<V>, indices: index::IndicesSource<'a>) -> Mesh<'a, V>{
+        let vertices = self.vertex_buffer(shape);
+        Mesh{
+            vertices,
+            indices
+        }
+    }
+    pub fn draw<V: Vertex, U: Uniforms>(&mut self, mesh: &Mesh<V>, program: &Program, uniforms: &U, draw_parameters: &DrawParameters) {
+        match &mut self.frame {
+            Some(frame) => frame.draw(&mesh.vertices, mesh.indices.clone(), program, uniforms, draw_parameters).unwrap(),
+            None => panic!("called 'draw()' without creating a frame first."),
+        };
+    }
+    ///Draw as mesh using a shader.
+    pub fn draw_with_shader<V: Vertex, U: Uniforms>(&mut self, mesh: &Mesh<V>, shader: &dyn Shader<U>) {
+        self.draw(mesh, shader.program(), &shader.uniforms(), &shader.draw_parameters());
+    }
+    
+}
+
+
+
+
+
+#[derive(Copy, Clone)]
+pub struct Vec2 {
+    position: [f32; 2],
+}
+
+#[allow(dead_code)]
+impl Vec2{
+    pub fn new(x: f32, y: f32) -> Self{
+        Self { position: [x, y] }
+    }
+    pub fn x(&mut self) -> &mut f32{
+        &mut self.position[0]
+    }
+    pub fn y(&mut self) -> &mut f32{
+        &mut self.position[1]
+    }
+}
+pub fn vec2(x: f32, y: f32) -> Vec2 {
+    Vec2::new(x, y)
+}
+implement_vertex!(Vec2, position);
+implement_uniform_block!(Vec2, position);
+
+#[derive(Copy, Clone)]
+pub struct Colour {
+    red: f32,
+    green: f32,
+    blue: f32,
+    alpha: f32
+}
+impl AsUniformValue for Colour{
+    fn as_uniform_value(&self) -> glium::uniforms::UniformValue<'_> {
+        glium::uniforms::UniformValue::Vec4([self.red, self.green, self.blue, self.alpha])
+    }
+}
+impl Colour{
+    pub fn new(r: f32, g: f32, b: f32, a: f32) -> Self {
+        Self {
+            red: r,
+            green: g,
+            blue: b,
+            alpha: a,
+        }
+    }
+}
+implement_vertex!(Colour, red, green, blue, alpha);
+
+pub struct Mesh<'a, V: Vertex> {
+    vertices: VertexBuffer<V>,
+    indices: index::IndicesSource<'a>
 }
